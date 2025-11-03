04591fc7e60ece13f429d257062944b579ad9e52
s-aura-bhhh <saurabh24860@gmail.com>
2025-11-03 05:05:39 -0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ app.use(cors());
 
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors{
+  cors: {
     origin: [
       "http://localhost:5173", // local dev
       "https://ezbuzzer-plus.vercel.app" // deployed frontend
